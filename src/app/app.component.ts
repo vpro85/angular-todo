e15f5fad879b8f0baabaaffe2898b52858b2477f
Vadim Prokoshin <vpro85@gmail.com>
2024-04-20 12:54:24 +0300

    2.12. Adding dependencies in the app.component.ts file in the src folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 import { TodoList } from './todoList';
 import { TodoItem } from './todoItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
